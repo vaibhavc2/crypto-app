4fad400297ca0e8a7aa6d81b2b9befaf2f55191e
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-09-10 13:19:46 +0530

    home almost ready

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,8 +1,10 @@
 import {
   Badge,
   Box,
+  Button,
   Container,
   HStack,
+  Heading,
   Image,
   Progress,
   Radio,
@@ -21,6 +23,7 @@ import axios from "axios";
 import { server } from "../main";
 import { useParams } from "react-router-dom";
 import ErrorComponent from "./ErrorComponent";
+import Chart from "./Chart";
 
 const CoinDetails = () => {
   const [coin, setCoin] = useState({});
@@ -28,8 +31,61 @@ const CoinDetails = () => {
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [currency, setCurrency] = useState("inr");
+  const [days, setDays] = useState("24h");
+  const [chartArray, setChartArray] = useState([]);
 
   const params = useParams();
+
+  const btns = ["24h", "7d", "14d", "30d", "60d", "200d", "1y", "max"];
+
+  const switchChartStats = (val) => {
+    switch (val) {
+      case "24h":
+        setDays("24h");
+        setLoading(true);
+        break;
+
+      case "7d":
+        setDays("7d");
+        setLoading(true);
+        break;
+
+      case "14d":
+        setDays("14d");
+        setLoading(true);
+        break;
+
+      case "30d":
+        setDays("30d");
+        setLoading(true);
+        break;
+
+      case "60d":
+        setDays("60d");
+        setLoading(true);
+        break;
+
+      case "200d":
+        setDays("200d");
+        setLoading(true);
+        break;
+
+      case "1y":
+        setDays("365d");
+        setLoading(true);
+        break;
+
+      case "max":
+        setDays("max");
+        setLoading(true);
+        break;
+
+      default:
+        setDays("24h");
+        setLoading(true);
+        break;
+    }
+  };
 
   const currencySymbol =
     currency === "inr" ? "₹" : currency === "eur" ? "€" : "$";
@@ -38,7 +94,11 @@ const CoinDetails = () => {
     const fetchCoinDetails = async () => {
       try {
         const { data } = await axios.get(`${server}/coins/${params.id}`);
+        const { data: chartData } = await axios.get(
+          `${server}/coins/${params.id}/market_chart?vs_currency=${currency}&days=${days}`
+        );
         setCoin(data);
+        setChartArray(chartData.prices);
         setLoading(false);
       } catch (err) {
         setError(true);
@@ -47,7 +107,7 @@ const CoinDetails = () => {
       }
     };
     fetchCoinDetails();
-  }, [params.id]);
+  }, [params.id, currency, days]);
 
   if (error) return <ErrorComponent msg={errorMessage} />;
 
@@ -57,11 +117,14 @@ const CoinDetails = () => {
         <Loader />
       ) : (
         <>
-          <Box width={"full"} borderWidth={1}>
-            {coin.name}
+          <Box width={"full"} borderWidth={1} mt={35}>
+            <Heading display={"block"} textAlign={"center"} p={2}>
+              {coin.name}
+            </Heading>
+            <Chart arr={chartArray} currency={currencySymbol} days={days} />
           </Box>
 
-          <RadioGroup value={currency} onChange={setCurrency} p={8}>
+          <RadioGroup value={currency} onChange={setCurrency} m={5}>
             <HStack spacing={4}>
               <Radio value="inr">₹ INR</Radio>
               <Radio value="usd">$ USD</Radio>
@@ -69,7 +132,13 @@ const CoinDetails = () => {
             </HStack>
           </RadioGroup>
 
-          {/** Button */}
+          <HStack p={4} overflowX={"auto"}>
+            {btns.map((i) => (
+              <Button key={i} onClick={() => switchChartStats(i)}>
+                {i}
+              </Button>
+            ))}
+          </HStack>
 
           <VStack spacing={4} p={"16"} alignItems={"flex-start"}>
             <Text fontSize={"small"} alignSelf={"center"} opacity={0.7}>
@@ -110,7 +179,23 @@ const CoinDetails = () => {
             />
 
             <Box w={"full"} p={4}>
-              <Item title="Max Supply" value={2323} />
+              <Item title="Max Supply" value={coin.market_data.max_supply} />
+              <Item
+                title="Circulating Supply"
+                value={coin.market_data.circulating_supply}
+              />
+              <Item
+                title="Market Cap"
+                value={`${currencySymbol} ${coin.market_data.market_cap[currency]}`}
+              />
+              <Item
+                title="All Time Low"
+                value={`${currencySymbol} ${coin.market_data.atl[currency]}`}
+              />
+              <Item
+                title="All Time High"
+                value={`${currencySymbol} ${coin.market_data.ath[currency]}`}
+              />
             </Box>
           </VStack>
         </>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,28 @@
+import { Box, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import btc from "../assets/btc.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"}>
+      <Image
+        src={btc}
+        w={"full"}
+        h={"86%"}
+        objectFit={"contain"}
+        filter={"grayscale(1)"}
+      />
+      <Text
+        fontSize={"6xl"}
+        textAlign={"center"}
+        fontWeight={"thin"}
+        color={"whiteAlpha.700"}
+        // mt={"-20"}
+      >
+        DigiCoin
+      </Text>
+    </Box>
+  );
 };
 
 export default Home;
